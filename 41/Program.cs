9d270020b07538d37f72ee321416c2a61a947550
Fortunella <sherbakovdaniil6@gmail.com>
2022-11-19 22:30:47 +0300

    Решено 6 домашнее задание

---
+++ 41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите количество чисел");
+int countInt = Convert.ToInt32(Console.ReadLine());
+
+int n;
+int sum = 0;
+Console.WriteLine("Введите числа");
+for (int i = 0; i < countInt; i++)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > 0) sum++;
+}
+
+Console.WriteLine("Количество положительных чисел");
+Console.WriteLine(sum);
---
+++ 42/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите k1");
+float k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b1");
+float b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+float k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2");
+float b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2) Console.WriteLine("Графики лежат на 1 линии");
+else if (k1 == k2) Console.WriteLine("Графики параллельны");
+else
+{
+    float x = (b2 - b1) / (k1 - k2);
+    float y = k1 * x + b1;
+    Console.WriteLine($"X:{x} Y:{y}");
+}
